a3e275c3c517dca14817eb2197670c44121a5cbb
Irgivensa12 <159887639+Irgivensa12@users.noreply.github.com>
2025-06-12 07:52:26 +0700

    Sesuaikan route megnarah ke profile buatan sendiri

--- project/routes/web.php
+++ project/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PemesananController;
 use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\UserController;
 
 Route::redirect('/', '/login');
 
@@ -11,10 +12,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+    Route::post('/profile/update', [UserController::class, 'updateProfile'])->name('profile.updateProfile');
+    Route::post('/profile/change-password', [UserController::class, 'ubahPassword'])->name('profile.ubahPassword');
 });
 
 // Dynamic route for pemesanan based on name
