b26e1ba0d2798c573098a4883f1549699a887b03
Benedictus Ditus <151413523+Dtuss@users.noreply.github.com>
2025-06-17 21:23:34 +0700

    Penambahan Halaman Pemesanan

--- project/routes/web.php
+++ project/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PemesananController;
 
 
 Route::get('/', function () {
@@ -36,3 +37,4 @@ Route::get('/profile', [UserController::class, 'profile'])->name('profile'); // 
 Route::put('/profile/update', [UserController::class, 'updateProfile'])->name('profile.updateProfile'); // rute untuk memperbarui profil pengguna
 Route::put('/profile/change-password', [UserController::class, 'ubahPassword'])->name('profile.ubahPassword'); // rute untuk mengubah password pengguna
 
+Route::get('/pemesanan/{name}', [PemesananController::class, 'show'])->name('pemesanan.show');
